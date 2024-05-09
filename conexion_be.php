1baa352c7a118f9db53f59fcfe1f6a9a925deb35
wuerineitor <162081169+wuerineitor@users.noreply.github.com>
2024-05-09 08:56:59 -0600

    Add files via upload

---
+++ conexion_be.php
@@ -0,0 +1,14 @@
+<?php
+
+
+    $conexion= mysqli_connect("localhost","root","","login_register_db");
+
+    /*/
+    if($conexion){
+        echo 'conectado a la base de datos';
+    }else{
+
+        echo 'no se conecto a base';
+    }
+    */
+
